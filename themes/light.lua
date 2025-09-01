69bf99f146fb7a721ce301250db662523cbb0f8a
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:24:40 +0700

    Create light.lua

---
+++ themes/light.lua
@@ -0,0 +1,7 @@
+return {
+    Name = "Light",
+    Primary = Color3.fromRGB(245,245,245),
+    Secondary = Color3.fromRGB(220,220,220),
+    Accent = Color3.fromRGB(0,120,255),
+    Text = Color3.fromRGB(0,0,0)
+}
