66c4eb7b942ba3cafafdd70938b13894d433c90c
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:25:13 +0700

    Create pastel.lua

---
+++ themes/pastel.lua
@@ -0,0 +1,7 @@
+return {
+    Name = "Pastel",
+    Primary = Color3.fromRGB(255,228,225),
+    Secondary = Color3.fromRGB(255,240,245),
+    Accent = Color3.fromRGB(255,182,193),
+    Text = Color3.fromRGB(50,50,50)
+}
