1ff3ac5dd7e9b374a5d421e32342cfcd9cdd2461
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:26:15 +0700

    Create classic.lua

---
+++ themes/classic.lua
@@ -0,0 +1,7 @@
+return {
+    Name = "Classic",
+    Primary = Color3.fromRGB(35,35,35),
+    Secondary = Color3.fromRGB(50,50,50),
+    Accent = Color3.fromRGB(0,120,255),
+    Text = Color3.fromRGB(255,255,255)
+}
