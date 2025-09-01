41e9c82e5c91e1ee4c4c6cadde96b6047cdd94d5
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:25:33 +0700

    Create retro.lua

---
+++ themes/retro.lua
@@ -0,0 +1,7 @@
+return {
+    Name = "Retro",
+    Primary = Color3.fromRGB(50,50,50),
+    Secondary = Color3.fromRGB(100,100,100),
+    Accent = Color3.fromRGB(255,165,0),
+    Text = Color3.fromRGB(200,200,200)
+}
