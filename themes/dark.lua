00a90e60aedc99d7f8acc0f5b4d0ffea36e495a7
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:24:23 +0700

    Create dark.lua

---
+++ themes/dark.lua
@@ -0,0 +1,7 @@
+return {
+    Name = "Dark",
+    Primary = Color3.fromRGB(30,30,30),
+    Secondary = Color3.fromRGB(45,45,45),
+    Accent = Color3.fromRGB(0,170,255),
+    Text = Color3.fromRGB(255,255,255)
+}
