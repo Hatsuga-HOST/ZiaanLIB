cfe7a3eb3fc4c90c8728e7e8682d5b3d61d48a88
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:26:31 +0700

    Create custom.lua

---
+++ themes/custom.lua
@@ -0,0 +1,7 @@
+return {
+    Name = "Custom",
+    Primary = Color3.fromRGB(255,255,255),
+    Secondary = Color3.fromRGB(200,200,200),
+    Accent = Color3.fromRGB(0,120,255),
+    Text = Color3.fromRGB(0,0,0)
+}
