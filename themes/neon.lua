9fb4fbe0d05e573bc6535f22f7a59c352f61e3a4
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:24:57 +0700

    Create neon.lua

---
+++ themes/neon.lua
@@ -0,0 +1,7 @@
+return {
+    Name = "Neon",
+    Primary = Color3.fromRGB(0,0,0),
+    Secondary = Color3.fromRGB(25,25,25),
+    Accent = Color3.fromRGB(0,255,255),
+    Text = Color3.fromRGB(255,0,255)
+}
