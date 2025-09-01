d2745a4635443d05f67c3a237604a0048b932bea
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:25:53 +0700

    Create cyberpunk.lua

---
+++ themes/cyberpunk.lua
@@ -0,0 +1,7 @@
+return {
+    Name = "Cyberpunk",
+    Primary = Color3.fromRGB(10,10,10),
+    Secondary = Color3.fromRGB(25,0,40),
+    Accent = Color3.fromRGB(255,0,200),
+    Text = Color3.fromRGB(0,255,255)
+}
