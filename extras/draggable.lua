3b4399722ec4891221db378bfc73e1b40afacd9e
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-01 15:18:29 +0700

    Create draggable.lua

---
+++ extras/draggable.lua
@@ -0,0 +1,41 @@
+-- extras/draggable.lua
+local UserInputService = game:GetService("UserInputService")
+local Draggable = {}
+
+function Draggable:Make(frame, dragTarget)
+    dragTarget = dragTarget or frame
+    local dragging, dragInput, dragStart, startPos
+
+    local function update(input)
+        local delta = input.Position - dragStart
+        frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+                                   startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+    end
+
+    dragTarget.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = true
+            dragStart = input.Position
+            startPos = frame.Position
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragging = false
+                end
+            end)
+        end
+    end)
+
+    dragTarget.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement then
+            dragInput = input
+        end
+    end)
+
+    UserInputService.InputChanged:Connect(function(input)
+        if input == dragInput and dragging then
+            update(input)
+        end
+    end)
+end
+
+return Draggable
